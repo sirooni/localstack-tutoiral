a32dc0381c524102eeb6850e07dcf10048b25ca8
sirooni <82398008+sirooni@users.noreply.github.com>
2025-04-03 03:04:29 +0900

    feature: アイテム追加API

--- server.ts
+++ server.ts
@@ -3,6 +3,11 @@ import {
   type CreateTableCommandInput,
   DynamoDBClient,
 } from "@aws-sdk/client-dynamodb"
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  type PutCommandInput,
+} from "@aws-sdk/lib-dynamodb"
 import express, { Request, Response } from "express"
 
 const PORT = 3000
@@ -20,6 +25,7 @@ const dynamoDB = new DynamoDBClient({
     secretAccessKey: "test",
   },
 })
+const docClient = DynamoDBDocumentClient.from(dynamoDB)
 
 // DynamoDB テーブル作成 API
 app.get("/create-table", async (_req: Request, res: Response) => {
@@ -38,6 +44,23 @@ app.get("/create-table", async (_req: Request, res: Response) => {
   } catch (error) {
     console.error("Error creating table: ", error)
     res.status(500).send("Error creating table")
+  }
+})
+
+// DynamoDB アイテム追加 API
+app.post("/add-item", express.json(), async (req: Request, res: Response) => {
+  const { id, task } = req.body
+
+  const params: PutCommandInput = {
+    TableName: TABLE_NAME,
+    Item: { id, task, completed: false },
+  }
+
+  try {
+    await docClient.send(new PutCommand(params))
+  } catch (error) {
+    console.error("Error adding item: ", error)
+    res.status(500).send("Error adding item")
   }
 })
 
